a8b4c2e0f8e6e6726e3740089797c0d43c536276
SmutnyJan <57211415+SmutnyJan@users.noreply.github.com>
2022-02-12 21:44:20 +0100

    advanced version

---
+++ lightgate.ts
@@ -0,0 +1,70 @@
+/**
+ * Použijte tento soubor k definování personalizovaných funkcí a bloků.
+ * Přečtěte si více na https://makecode.microbit.org/blocks/custom
+ */
+
+
+/**
+ * Custom blocks
+ */
+//% weight=100 color=#a0a803 icon="\uf030"
+namespace Svetelna_Brana {
+    let toleration = 0
+    let calibrated = false
+    let calibrationBegan = false
+    let lightLevel = 0
+
+    /**
+    * Nastaví novou toleraci
+    */
+    //% block="Nastav toleranci %tolerace"
+
+    export function NastavitToleranci(tol: number): void {
+        toleration = tol;
+    }
+
+    /**
+    * Spustí kalibraci
+    */
+    //% block="Zkalibruj za %cas vteřin"
+
+    export function SpustitKalibraci(seconds: number): void {
+        calibrated = false
+        led.stopAnimation()
+        music.stopAllSounds()
+        calibrationBegan = true
+        while (seconds > 0) {
+            basic.showNumber(seconds)
+            basic.pause(1000)
+            seconds += -1
+        }
+        lightLevel = input.lightLevel()
+        calibrated = true
+    }
+
+    /**
+    * Zkontroluje hladinu světla
+    */
+    //% block="Proveď kontrolu"
+
+    export function ProvedKontrolu(): boolean {
+        if (calibrated == true && (input.lightLevel() > lightLevel + toleration || input.lightLevel() < lightLevel - toleration)) {
+            return true
+        } else if (calibrationBegan == false) {
+            basic.showString("Zkalibrujte senzor!")
+        } else if (calibrated == true) {
+            basic.showIcon(IconNames.Happy)
+        }
+        return false
+    }
+
+
+
+
+
+
+
+
+
+
+}
--- main.ts
+++ main.ts
@@ -1,1 +1,16 @@
-
+input.onButtonPressed(Button.A, function () {
+    Svetelna_Brana.SpustitKalibraci(5)
+})
+Svetelna_Brana.NastavitToleranci(25)
+basic.forever(function () {
+    if (Svetelna_Brana.ProvedKontrolu() == true) {
+        music.playTone(262, music.beat(BeatFraction.Whole))
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            . . . . .
+            . # . # .
+            # . # . #
+            `)
+    }
+})
